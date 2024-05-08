73b809548a02658a7c000552a2098ba6b7d91cb3
marcelocho <marcelo.cho@gmail.com>
2024-05-07 22:18:09 -0300

    fim talvez?

--- .eleventy.js
+++ .eleventy.js
@@ -5,20 +5,33 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
-  eleventyConfig.addFilter("postDate", dateObj => {
+  eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_SHORT);
   });
+  
+  eleventyConfig.addNunjucksFilter("excludeFromCollection", function (collection=[], pageUrl=this.ctx.page.url) {
+    return collection.filter(post => post.url !== pageUrl);
+  });
+
+  eleventyConfig.addFilter(
+    "filterByTags",
+    function (collection = [], requiredTags) {
+      const filterTags = requiredTags.filter((tag) => tag !== "post");
+      return collection.filter((post) => {
+        return filterTags.some((tag) => post.data.tags?.includes(tag));
+      });
+    },
+  );
 
   eleventyConfig.addNunjucksFilter("limit", (arr, limit) => {
     // console.log(arr)
-    return arr?.slice(0, limit)
-  }
-  );
+    return arr?.slice(0, limit);
+  });
 
   return {
     dir: {
       input: "src",
-      output: "public"
-    }
+      output: "public",
+    },
   };
 };
